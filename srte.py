094c72d84b3e85afae96be91282b736371b894e2
Abdelrahman Fouad <abderhmana@gmail.com>
2026-06-11 19:34:22 +0300

    Update srte.py

--- srte.py
+++ srte.py
@@ -8,8 +8,7 @@ from google.genai import types
 
 # --- Configure Gemini API ---
 MODELS = [
-    "gemini-2.5-flash", 
-    "gemini-3.1-flash-lit", 
+    "gemini-2.5-flash-lite",  
     "gemini-3.1-flash",
     "gemini-3.5-flash",
     "gemini-3.1-flash-lite",
